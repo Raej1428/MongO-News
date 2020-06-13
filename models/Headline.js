1168861cb60d41b62d21aaeb425e40a85910ba8a
raej1428 <rachaelgladwin@Rachaels-iMac.local>
2020-06-13 09:39:55 -0600

    working on deployong app to heroku, adde controllers and scripts to get app working and added saved script to saved file.

---
+++ models/Headline.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+const Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new Headlinechema object
+const HeadlineSchema = new Schema({
+  title: {
+    type: String,
+    unique: { index: { unique: true } },
+    required: true
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: false
+  },
+  imgUrl: {
+    type: String,
+    required: false
+  },
+  // headline set to unsaved initially
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+   // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Headline with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: `Note`
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+const Headline = mongoose.model("Headline", HeadlineSchema);
+
+// Export the Headline model
+module.exports = Headline;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+// Require all models
+const db = require("./models");
+
+const PORT = process.env.PORT || 8080;
+
+// Initialize Express
+const app = express();
+
+// Use morgan logger for logging requests
+app.use(logger('dev'));
+
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Use body-parser for handling form submissions
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Routes
+const routes = require("./routes");
+// All requests go through routes
+app.use(routes);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+  });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
